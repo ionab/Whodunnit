67fbfc2a184d662930fa71b0fb67f8367252bcd2
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-04 16:03:45 +0100

    inital commit

---
+++ whodunnit.js
@@ -0,0 +1,91 @@
+// Episode 1
+//will return "My name is Bob"
+var name = 'Bob';
+
+
+var printName = function() {
+  console.log('My name is ' + name );
+};
+
+printName();
+
+// Episode 2
+// will return 3
+
+score = 5;
+
+var result = function() {
+  var score = 3;
+  return score;
+};
+
+console.log(result());
+
+// Episode 3
+// will print out a list of index positions with their respective positions
+
+var myAnimals = ['Chickens', 'Cats', 'Rabbits'];
+
+var listAnimals = function() {
+  myAnimals = ['Ducks', 'Dogs', 'Lions'];
+  for(var i=0;i<myAnimals.length; i++){
+    console.log(i + ": " + myAnimals[i]);
+  }
+}
+
+listAnimals();
+
+// Episode 4
+// suspectThree will print out as Colin when you run the allSuspects() method.
+//suspectThree will be Upul on line 53.
+
+var suspectOne = 'Ally';
+var suspectTwo = 'Alan';
+var suspectThree = 'Upul';
+var suspectFour = 'Alistair';
+
+var allSuspects = function() {
+  var suspectThree = 'Colin'
+  console.log('Suspects include: ' + suspectOne + ', ' + suspectTwo + ', ' + suspectThree + ', ' + suspectFour) //here Colin
+};
+
+allSuspects();
+console.log( 'Suspect three is:' + suspectThree ); //here Upul
+
+
+// Episode 5
+// name will be changed to Poirot
+var detective = {
+  name : 'Ace Ventura',
+  pet : 'monkey'
+};
+
+var printName = function(detective) {
+  return detective.name
+};
+
+var detectiveInfo = function() {
+  detective['name'] = 'Poirot'
+  return printName(detective);
+};
+
+console.log(detectiveInfo());
+
+
+// Episode 6
+// The murderer is Jerry
+
+var murderer = 'Rick';
+
+var outerFunction = function() {
+  var murderer = 'Morty';
+
+  var innerFunction = function() {
+    murderer = 'Jerry';
+  }
+
+  innerFunction();
+}
+
+outerFunction();
+console.log('the murderer is ', murderer);
